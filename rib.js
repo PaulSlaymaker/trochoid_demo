6cd0935531a2f51aa53b4f17424802babe32fcb8
Paul Slaymaker <paul25882@gmail.com>
2019-07-01 18:53:21 -0500

    first finish

--- rib.js
+++ rib.js
@@ -35,6 +35,61 @@ function getRandomInt(min, max, low) {
   max=Math.floor(max);
   return Math.floor(powerRandom(p)*(max-min))+min;
 }
+
+var primes=[2,3,5,7,11,13,17,19];
+
+var cycsets={
+  2:[2,4,6,8,10,12,14,16,18],
+  3:[3,6,9,12,15,18],
+  5:[5,10,15],
+  7:[7,14],
+  11:[11],
+  13:[13],
+  17:[17],
+  19:[19]
+};
+
+var colsets={
+  2:[2,4,6,8],
+  3:[3,6,9],
+  5:[5],
+  7:[7]
+};
+
+function getFactors(n) {
+  let a=[];
+  for (let p of primes) {
+    if (n%p==0) {
+      a.push(p);
+    }
+  }
+  return a;
+}
+
+function getCycleArray(n) {
+  let a=new Set();
+  for (let p of primes) {
+    if (n%p==0) {
+      for (let f of cycsets[p]) {
+        a.add(f);
+      }
+    }
+  }
+  return Array.from(a);
+}
+
+function getColorArray(n) {
+  let a=new Set();
+  for (let i=0; i<4; i++) {
+    if (n%primes[i]==0) {
+      for (let f of colsets[primes[i]]) {
+        a.add(f);
+      }
+    }
+  }
+  return Array.from(a);
+}
+
 var Roulette=function(ro) {
   if (ro instanceof Roulette) {
     Object.assign(this, ro);
@@ -83,9 +138,11 @@ var Roulette=function(ro) {
     //rself.c4=rself.setCycles();
   }
   this.getCycle0Match=function() {
+    return getRandomInt(1,19,4);
+/*
     switch (rself.cycleSet) {
       case 2:
-	return [1,3,5,7,9,11,13,15,17,19][getRandomInt(0,10,4)];
+	return [1,3,5,7,9,11,13,15,17,19][getRandomInt(0,10,3)];
       case 3:
 	return [1,2,4,5,7,8,10,11,13,14,16,17,19][getRandomInt(0,13,4)];
       case 4:
@@ -112,19 +169,36 @@ var Roulette=function(ro) {
       case 15:
 	return [1,2,4,7,8,11,13,14,16,17,19][getRandomInt(0,11,2)];
       case 16:
-	return [1,3,5,7,9,11,13,15,17,19][getRandomInt(0,10)];
+	return [1,3,5,7,9,11,13,15,17,19][getRandomInt(0,10,2)];
       case 17:
-	return [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,18,19][getRandomInt(0,18)];
+	return [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,18,19][getRandomInt(0,18,2)];
       case 18:
-	return [1,5,7,11,13,17,19][getRandomInt(0,7)];
+	return [1,5,7,11,13,17,19][getRandomInt(0,7,2)];
       case 19:
-	return [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18][getRandomInt(0,18)];
+	return [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18][getRandomInt(0,18,2)];
     }
     debugger;
     return false;
+*/
   }
- 
+
   this.setCycles=function() {
+    if (Math.random()<0.7) {  // put symmetry control here
+      let ca=getFactors(ribbons.rCount);
+      if (ca.length==0) {
+	return this.cycleSet;
+      }
+      return ca[getRandomInt(0,ca.length)];
+    } else {
+      let ca=getCycleArray(ribbons.rCount);
+      if (ca.length==0) {
+	return this.cycleSet;
+      }
+      return ca[getRandomInt(0,ca.length)];
+    }
+  }
+
+  this.setCyclesX=function() {
     if (this.cycleSet==4) {
       return [2,4,8,16][getRandomInt(0,4)];
     } else if (this.cycleSet==5) {
@@ -151,27 +225,43 @@ var Roulette=function(ro) {
       return this.cycleSet;
     }
   }
+
   this.randomizeRadiiCount=function() {
     rself.radiiCount=2+Math.round(2*Math.random());
   }
   this.randomizeRadii=function() {
     if (rself.radiiCount==4) {
       rself.r1=70-20*Math.random();
+/*
       rself.r2=70-20*Math.random();
       rself.r3=70-20*Math.random();
       rself.r4=70-20*Math.random();
+*/
+//      rself.r1=20+160*Math.random();
+      rself.r2=20+(240-rself.r1)*Math.random();
+      rself.r3=20+(280-rself.r1-rself.r2)*Math.random();
+      rself.r4=280-rself.r1-rself.r2-rself.r3;
     } else if (rself.radiiCount==3) {
       rself.r1=90-30*Math.random();
+/*
       rself.r2=90-30*Math.random();
       rself.r3=90-30*Math.random();
+*/
+//      rself.r1=20+160*Math.random();
+      rself.r2=20+(240-rself.r1)*Math.random();
+      rself.r3=(280-rself.r1-rself.r2);
       rself.r4=0;
     } else if (rself.radiiCount==2) {
       let rr=210+70*Math.random();  // sum of radii from 210-280
       let rd=30-60*Math.random();
       rself.r1=(rd+rr)/2;
       rself.r2=(rr-rd)/2;
+/*
+      rself.r1=20+160*Math.random();
+      rself.r2=280-rself.r1;
       rself.r3=0;
       rself.r4=0;
+*/
     } else {
       debugger;
     }
@@ -201,6 +291,42 @@ var Roulette=function(ro) {
     rself.type4=[-1,1][getRandomInt(0,2)];
 */
   }
+  this.getSP=function() {
+    let sp=this.c0+this.c1;
+    switch(this.radiiCount) {
+      case 2:
+	return sp;
+      case 3:
+	return sp+=this.c2;
+      case 4:
+	return sp+=(this.c2+this.c3);
+      default:
+	return 0;
+    }
+  }
+  this.controlledCycleChange=function(mod) {
+    let count=0;
+    let sMax=30;
+    let sMin=5;
+    do {
+      mod();
+      var sp=rself.getSP();
+      if (count++>10) {
+        return;
+      }
+    //} while (sp<rself.spMin || sp>rself.spMax);
+    } while (sp>sMax++ || sp<sMin--);
+  }
+}
+
+function cbLoc(p1,p2,frac) {
+  var f1=.2;
+  var f2=.8;
+  var e1=Math.pow(1-frac,3)*p1;
+  var e2=3*frac*Math.pow(1-frac,2)*(p1+(p2-p1)*f1);
+  var e3=3*(1-frac)*Math.pow(frac,2)*(p1+(p2-p1)*f2);
+  var e4=Math.pow(frac,3)*p2;
+  return e1+e2+e3+e4;
 }
 
 var path={
@@ -208,46 +334,51 @@ var path={
   toRo:new Roulette(),
   start:0,
   frac:0,
+  //duration:20000,
   duration:5000,
+  //duration:2000,
   getMetrics:function(rotFrac,n) {
     let fromMet=this.fromRo.getMetrics(rotFrac,n);
     let toMet=this.toRo.getMetrics(rotFrac,n);
     return {
-      x:this.frac*toMet.x+(1-this.frac)*fromMet.x,
-      y:this.frac*toMet.y+(1-this.frac)*fromMet.y,
+      //x:this.frac*toMet.x+(1-this.frac)*fromMet.x,
+      //y:this.frac*toMet.y+(1-this.frac)*fromMet.y,
+      x:cbLoc(fromMet.x,toMet.x,this.frac),
+      y:cbLoc(fromMet.y,toMet.y,this.frac)
     }
   },
+  mf:0.3,
   transit:function() {
     Object.assign(this.fromRo, this.toRo);
     this.toRo=new Roulette(this.toRo);
     let lf=0.1;
-    let mf=0.3;
+    //let mf=0.3;
     let hf=0.7;
-    if (Math.random()<lf) {
+    if (Math.random()<this.mf) {
       this.toRo.randomizeCycles();
       //this.toRo.controlledCycleChange(this.toRo.randomizeCycles, 'cyc');
     } else { 
-      if (Math.random()<mf) {
-	this.toRo.c0=this.toRo.getCycle0Match();
-        //this.toRo.controlledCycleChange(this.toRo.getCycle0Match, 'c0');
+      if (Math.random()<this.mf) {
+	//this.toRo.c0=this.toRo.getCycle0Match();
+        this.toRo.controlledCycleChange(this.toRo.getCycle0Match);
       }
-      if (Math.random()<mf) {
-	this.toRo.softCycle();
-        //this.toRo.controlledCycleChange(this.toRo.softCycle, 'ss');
+      if (Math.random()<this.mf) {
+	//this.toRo.softCycle();
+        this.toRo.controlledCycleChange(this.toRo.softCycle);
       }
     }
-    if (Math.random()<mf) {
-      this.toRo.randomizeRadiiCount();
-      //this.toRo.controlledCycleChange(this.toRo.randomizeRadiiCount, 'rrc');
+    if (Math.random()<this.mf) {
+      //this.toRo.randomizeRadiiCount();
+      this.toRo.controlledCycleChange(this.toRo.randomizeRadiiCount);
     }
-    if (Math.random()<hf) {
+    if (Math.random()<this.mf) {
       this.toRo.randomizeTypes();
     }
-    if (Math.random()<lf) {
+    if (Math.random()<this.mf/4) {
       this.toRo.dz=[-1,1][getRandomInt(0,2)];
     }
     this.toRo.randomizeRadii();
-    setTable();
+setTable();
   },
   animate:function(ts) {
     if (stopped) {
@@ -287,21 +418,31 @@ var fade={
 }
 
 var ribbons={
-  rCount:4+Math.round(40*Math.pow(Math.random(),1.8)),
+  rCount:4+Math.round(40*Math.pow(Math.random(),1.7)),
   DS:0, // rotational increment to next line
+  //DSinc:0.0002,
+  //DSinc:0.00015,
+  DSinc:0.0002,
   rcTrans:'N',   // N,D,H
   colorCount:2,
   hStart:0,
   hDuration:24000,
   hFrac:0,
-  XT1:280*(1-2*Math.random()),
-  YT1:280*(1-2*Math.random()),
+  XT1:120*(1-2*Math.random()),
+  YT1:120*(1-2*Math.random()),
   tStart:0,
   tDuration:8000,
   tFrac:0,
   fromHues:[Math.round(Math.random()*360)],
   toHues:[Math.round(Math.random()*360)],
   goodCycleSet:function() {
+    //let ca=getCycleArray(ribbons.rCount);
+    let ca=getFactors(ribbons.rCount);
+    if (ca.length==0) {
+      return getRandomInt(4,20,2);
+    }
+    return ca[getRandomInt(0,ca.length)];
+/*
     switch (ribbons.rCount) {
       case 4:
       case 8:
@@ -326,7 +467,7 @@ var ribbons={
       case 18:
 	return [6,9,18][getRandomInt(0,3)]; // etc 
       case 20:
-        return [5,10][getRandomInt(0,2)];  // 8,16
+        return [4,5,10][getRandomInt(0,2)];  // 8,16
       case 21:
         return 7;
       case 22:
@@ -345,6 +486,16 @@ var ribbons={
         return 11;
       case 34:
         return 17;
+      case 35:
+        return [5,7][getRandomInt(0,2)];
+      case 36:
+        return [4,6,9,12,18][getRandomInt(0,5)];
+      case 38:
+        return 19;
+      case 39:
+        return 13;
+      case 40:
+        return [4,5,8,10][getRandomInt(0,4)];
     }
     if (ribbons.rCount<20) {
       return ribbons.rCount;
@@ -359,8 +510,16 @@ var ribbons={
         return getRandomInt(4,20,2);
       }
     }
+*/
   },
   goodColorCount:function() {
+/*
+    let ca=getColorArray(ribbons.rCount);
+    if (ca.length==0) {
+      return 2;
+    }
+    return ca[getRandomInt(0,ca.length)];
+*/
     switch (ribbons.rCount) {
       case 4:
 	return [2,4][Math.round(Math.random())];
@@ -397,7 +556,7 @@ var ribbons={
       case 35:
 	return [5,7][Math.round(Math.random())];
       case 36:
-	return [2,3,4,6,8][getRandomInt(0,5)];
+	return [2,3,4,6][getRandomInt(0,4)];
       case 40:
 	return [2,4,5,8][getRandomInt(0,4)];
       default:
@@ -405,6 +564,10 @@ var ribbons={
 	  return 2;
 	} else if (ribbons.rCount%3==0) { 
 	  return 3;
+	} else if (ribbons.rCount%5==0) { 
+	  return 5;
+	} else if (ribbons.rCount%7==0) { 
+	  return 7;
 	} else {
 	  return 2+Math.round(Math.random());
 	}
@@ -431,6 +594,7 @@ ribbons.toHues[0]=(ribbons.fromHues[0]+Math.random()*360/ribbons.colorCount)%360
     return (ribbons.hFrac*ribbons.toHues[n%ribbons.colorCount]+(1-ribbons.hFrac)*ribbons.fromHues[n%ribbons.colorCount])/2%360;
   },
   transitColor:function() {
+    if (colorCountRanger.lock) return;
     ribbons.fromHues=ribbons.toHues.slice();
     ribbons.toHues=[];
 if (ribbons.colorCount==2) {
@@ -444,18 +608,20 @@ ribbons.toHues[0]=(ribbons.fromHues[0]+Math.random()*360/ribbons.colorCount)%360
     }
   },
   randomize:function() {
-    ribbons.rCount=4+Math.round(50*Math.pow(Math.random(),1.8));
-    while ([11,13,17,19,23].includes(ribbons.rCount)) {
-      ribbons.rCount=4+Math.round(50*Math.pow(Math.random(),1.8));
+    ribbons.rCount=4+Math.round(55*Math.pow(Math.random(),1.7));
+    while ([11,13,17,19,23,29].includes(ribbons.rCount)) {
+      ribbons.rCount=4+Math.round(55*Math.pow(Math.random(),1.7));
     }
+    if (colorCountRanger!=undefined && colorCountRanger.lock) return;
     ribbons.colorCount=ribbons.goodColorCount();
     ribbons.randomizeHues();
   },
   transitCount:function() {
+    if (ribbonsRanger.lock) return;
     if (ribbons.rcTrans=='N') {
-      if (Math.random()<0.1) {
-        ribbons.XT1=280*(1-2*Math.random());
-        ribbons.YT1=280*(1-2*Math.random());
+      if (Math.random()<path.mf) {
+        ribbons.XT1=120*(1-2*Math.random());
+        ribbons.YT1=120*(1-2*Math.random());
         ribbons.rcTrans='T';
       }
     } else if (ribbons.rcTrans=='T') {
@@ -464,6 +630,8 @@ ribbons.toHues[0]=(ribbons.fromHues[0]+Math.random()*360/ribbons.colorCount)%360
       path.toRo.randomizeCycles();
       ribbons.rcTrans='R';
 setTable();
+ribbonsRanger.setValue(ribbons.rCount);
+colorCountRanger.setValue(ribbons.colorCount);
     } else if (ribbons.rcTrans=='R') {
       ribbons.rcTrans='N';
     }
@@ -548,7 +716,7 @@ let tz=Math.pow(1-ribbons.tFrac,5);
       ctx.stroke();
       ctx.closePath();
     }
-    ribbons.DS+=0.0002;
+    ribbons.DS+=ribbons.DSinc;
     if (!ribbons.hStart) {
       ribbons.hStart=ts;
     }
@@ -576,6 +744,10 @@ let tz=Math.pow(1-ribbons.tFrac,5);
     }
   }
 }
+
+function getR(x,y) {
+  return Math.pow((Math.pow(x,2)+Math.pow(y,2)),0.5);
+}
 /*
 while ([11,13,17,19,23].includes(ribbons.rCount)) {
 //if (ribbons.rCount==11 || ribbons.rCount==13) {
@@ -585,9 +757,6 @@ ribbons.colorCount=ribbons.goodColorCount();
 */
 ribbons.randomize();
 ribbons.randomizeHues();
-
-path.toRo.randomizeCycles();
-path.fromRo.randomizeCycles();
 
 /*
 var hues=[Math.round(Math.random()*360)];
@@ -613,6 +782,250 @@ function start() {
   }
 }
 canvas.addEventListener("click", start, false);
+
+var menu=new function() {
+  this.fdr=document.querySelectorAll('.bgfade');
+  this.mbut=document.querySelector('#pmenu');
+  this.mrep=document.querySelector('#pmrep');
+  this.xrep=this.mbut.children[0];
+  this.open=true;
+  this.cdiv=document.querySelector('#ctl');
+  let ms=this;
+  this.mbut.onclick=function() {
+    if (ms.open) {
+      ms.open=false;
+      ms.cdiv.style.display='block';
+      rotateSpeedRanger.report();
+      curveSpeedRanger.report();
+      ribbonsRanger.report();
+      colorCountRanger.report();
+      changeRateRanger.report();
+    } else {
+      ms.open=true;
+    }
+    requestAnimationFrame(ms.animate);
+  }
+  this.animate=function(ts) {
+    if (!ms.start) ms.start=ts;
+    let progress=ts-ms.start;
+    if (progress<400) {
+      let frac=progress/400;
+      if (ms.open) {
+	for (let x of ms.fdr) {
+	  x.style.background='hsl(0,0%,'+(1-frac)*96+'%)';
+	}
+	ms.mrep.style.opacity=frac*0.8+(1-frac)*0.001;
+	ms.xrep.style.opacity=(1-frac)*0.8+frac*0.001;
+	ms.cdiv.style.opacity=(1-frac)*0.8+frac*0.001;
+      } else {
+        for (let x of ms.fdr) {
+          x.style.background='hsl(0,0%,'+frac*96+'%)';
+        }
+        ms.mrep.style.opacity=0.001;
+        ms.xrep.style.opacity=0.8;
+        ms.cdiv.style.opacity=0.8;
+      }
+      requestAnimationFrame(ms.animate);
+    } else {
+      if (ms.open) {
+        ms.cdiv.style.display='none';
+      } else {
+        ms.cdiv.style.opacity=1;
+      }
+      ms.start=0;
+    }
+  }
+}();
+
+function bmov(btn) {
+  btn.parentNode.parentNode.style.color='blue';
+}
+
+function bmou(btn) {
+  btn.parentNode.parentNode.style.color='black';
+}
+
+function btnFocus(ctl) {
+  ctl.parentNode.parentNode.className='mb infoc';
+}
+
+function btnBlur(ctl) {
+  ctl.parentNode.parentNode.className='mb';
+}
+
+var Ranger=function(obj) {
+  this.box=document.createElement('div');
+  this.box.className='mb';
+  document.querySelector('#ctl').appendChild(this.box);
+  let spacer=document.createElement('div');
+  spacer.className='pholder';
+  this.box.appendChild(spacer);
+  this.slider=document.createElement('div');
+  this.slider.className='slider';
+  this.box.appendChild(this.slider);
+  this.displayDiv=document.createElement('div');
+  this.displayDiv.className='rtext';
+  this.box.appendChild(this.displayDiv);
+  let labeldiv=document.createElement('div');
+  labeldiv.className='rlabel';
+  labeldiv.textContent=obj.label;
+  this.displayDiv.appendChild(labeldiv);
+  this.valueDiv=document.createElement('div');
+  this.valueDiv.className='rep';
+  this.displayDiv.appendChild(this.valueDiv);
+  this.units=' ';
+  this.input=document.createElement('input');
+  this.input.type='range';
+  this.input.className='range';
+  //if (obj.hasOwnProperty('value')) { }
+  this.input.min=obj.min;
+  this.input.max=obj.max;
+  this.input.step=obj.step;
+  this.input.value=obj.value;
+  let inputdiv=document.createElement('div');
+  inputdiv.className='inputdiv';
+  inputdiv.appendChild(this.input);
+  this.box.appendChild(inputdiv);
+  this.max=parseFloat(this.input.max);
+  this.min=parseFloat(this.input.min);
+  let rself=this;
+  if (obj.hasOwnProperty('lockInput')) {
+    let lockdiv=document.createElement('div');
+    lockdiv.className='rep lockdiv';
+    inputdiv.appendChild(lockdiv);
+    this.lockRep=document.createElement('div');
+    this.lockRep.className='locksym';
+    this.lockRep.innerHTML='&#128275';
+    lockdiv.appendChild(this.lockRep);
+    this.lockCB=document.createElement('input');
+    this.lockCB.type='checkbox';
+    this.lockCB.className='cb';
+    this.lockCB.onmouseover=function() {
+      rself.lockRep.style.color='blue';
+    }
+    this.lockCB.onmouseout=function() {
+      rself.lockRep.style.color='black';
+    }
+    this.lockCB.onfocus=function() {
+      lockdiv.classList.add('infoc');
+    }
+    this.lockCB.onblur=function() {
+      lockdiv.classList.remove('infoc');
+    }
+    this.lockCB.onclick=function() {
+      rself.lock=rself.lockCB.checked;
+      rself.lockRep.innerHTML=rself.lock?'&#128274':'&#128275';
+      obj.lockInput(rself.lock);
+    }
+    lockdiv.appendChild(this.lockCB);
+  }
+  this.setValue=function(v) {
+    rself.input.value=v;
+    rself.report();
+  }
+  this.report=function() {
+    if (!menu.open) {
+      rself.slider.style.width=8+148*(rself.input.value-rself.min)/(rself.max-rself.min)+'px';
+      rself.valueDiv.textContent=rself.input.value+rself.units;
+    }
+  }
+  this.input.onmouseover=function() {
+    rself.slider.style.opacity=1;
+    rself.displayDiv.style.color='blue';
+  }
+  this.input.onmouseout=function() {
+    if (document.activeElement!=rself.input) {
+      rself.slider.style.opacity=0;
+    }
+    rself.displayDiv.style.color='black';
+  }
+  this.input.onfocus=function() {
+    inputdiv.classList.add('infoc');
+    rself.slider.style.opacity=1;
+  }
+  this.input.onblur=function() {
+    inputdiv.classList.remove('infoc');
+    rself.slider.style.opacity=0;
+  }
+  this.input.oninput=function() {
+    rself.report();
+    if (obj.hasOwnProperty('oninput')) {
+      obj.oninput(rself.input.value);
+    }
+    if (stopped) {
+      orbs.draw(1);
+    }
+  }
+}
+
+var rotateSpeedRanger=new Ranger({
+  label:'Rotational speed',
+/*
+  min:0.0001,
+  max:0.01,
+  step:0.0001,
+  value:0.0002,
+*/
+  min:1,
+  max:10,
+  step:1,
+  value:5,
+  oninput:function(val) {
+    //ribbons.DSinc=val*0.000025;
+    ribbons.DSinc=val*0.00005-0.000045;
+  },
+});
+
+var curveSpeedRanger=new Ranger({
+  label:'Curve speed',
+  min:1,
+  max:10,
+  step:1,
+  value:5,
+  oninput:function(val) {
+    path.duration=50000/val-4500;
+  },
+});
+
+var ribbonsRanger=new Ranger({
+  label:'Ribbons',
+  min:1,
+  max:200,
+  step:1,
+  value:50,
+  oninput:function(val) {
+    ribbons.rCount=parseInt(val);
+  },
+  lockInput:function(isLocked) { 
+    ribbons.rcTrans='N';
+  }
+});
+ribbonsRanger.setValue(ribbons.rCount);
+
+var colorCountRanger=new Ranger({
+  label:'Color count',
+  min:1,
+  max:100,
+  step:1,
+  value:5,
+  oninput:function(val) {
+    ribbons.colorCount=parseInt(val);
+    ribbons.randomizeHues();
+  },
+  lockInput:function(isLocked) { }
+});
+colorCountRanger.setValue(ribbons.colorCount);
+
+var changeRateRanger=new Ranger({
+  label:'Change rate',
+  min:1,
+  max:10,
+  step:1,
+  value:3,
+  oninput:function(val) {
+    path.mf=val/10;
+  }
+});
 
 var SR=function(obj) {
   let row=document.createElement('tr');
@@ -763,6 +1176,7 @@ var srs=[
       tt.textContent=path.toRo.r5.toFixed(0);
     }
   }),
+*/
   new SR({
     label:'SP',
     oc:function(ft,tt) {
@@ -770,7 +1184,6 @@ var srs=[
       tt.textContent=path.toRo.getSP();
     }
   }),
-*/
 ];
 
 function setTable() {
