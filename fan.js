66f5d53a34969f279c20738ae3e96bf3a29eeecf
Paul Slaymaker <paul25882@gmail.com>
2020-05-31 15:30:41 -0500

    mods

--- fan.js
+++ fan.js
@@ -1,39 +1,18 @@
 "use strict"; // Paul Slaymaker, paul25882@gmail.com
 const body=document.getElementsByTagName("body").item(0);
 body.style.background="black";
+body.style.margin="20";
 
-/*
-const canvas=(()=>{
+const ctx=(()=>{
+  let d=document.createElement("div");
+  d.style.textAlign="center";
+  body.append(d);
   let c=document.createElement("canvas");
   c.width="800";
   c.height="800";
-  body.append(c);
-  return c;
+  d.append(c);
+  return c.getContext("2d");
 })();
-*/
-const canvas=document.getElementById("cts");
-
-var ctx=canvas.getContext('2d');
-ctx.fillStyle="#4444AA";
-
-var fillContext=(()=>{
-  var osc=document.createElement("canvas");
-  osc.width=800;
-  osc.height=800;
-  return osc.getContext("2d");
-})();
-
-onresize=function() { 
-  let ss=ctx.strokeStyle;
-  let D=0.9*Math.min(window.innerWidth,window.innerHeight); 
-  canvas.width=D;
-  canvas.height=D;
-  fillContext.canvas.width=D;
-  fillContext.canvas.height=D;
-  P=D/2;
-  ctx.translate(P,P);
-  ctx.strokeStyle=ss;
-}
 
 var getRandomInt=(min,max,low)=>{
   if (low) {
@@ -43,6 +22,41 @@ var getRandomInt=(min,max,low)=>{
   }
 }
 
+var fillContext=(()=>{
+  var osc=document.createElement("canvas");
+  osc.width=800;
+  osc.height=800;
+  let fctx=osc.getContext("2d");
+  fctx.randColor=()=>{ 
+    return "hsl("+getRandomInt(0,360)+",70%,50%)";
+  }
+  fctx.setBackground=()=>{ 
+    fctx.fillStyle="hsl("+getRandomInt(0,360)+",70%,50%)";
+    fctx.fillRect(0,0,2*P,2*P);
+  }
+  fctx.setCircle=(tx)=>{ 
+    let RZ=Math.abs(P*Math.cos(tx)*Math.cos(Math.sin(W*tx)));    
+    fctx.beginPath();
+    fctx.arc(P,P,RZ,0,2*Math.PI);
+    fctx.closePath();
+    fctx.fillStyle="hsl("+getRandomInt(0,360)+",70%,50%)";
+    fctx.fill();
+  }
+  return fctx;
+})();
+
+onresize=function() { 
+  let ss=ctx.strokeStyle;
+  let D=0.9*Math.min(window.innerWidth,window.innerHeight); 
+  ctx.canvas.width=D;
+  ctx.canvas.height=D;
+  fillContext.canvas.width=D;
+  fillContext.canvas.height=D;
+  P=D/2;
+  ctx.translate(P,P);
+  ctx.strokeStyle=ss;
+}
+
 var O=0;
 var Q=1/16;
 var P=400;
@@ -50,18 +64,28 @@ var S=100;
 var W=2;
 var C=2;
 
-var RZ1=323.5;
+//var RZ1=323.5;
+//var RZ2=123.4;
 //RZ=P*Math.cos(n/(2*W))*Math.cos(Math.sin(n/(2*W)))    
 // W=5, t=0.1 and 0.2
 // W=4, t=0.125, just one radius
 // W=3, t=0.1666667, one radius: P/2
-var RZ2=123.4;
 
 var getPattern=function() {
-  fillContext.fillStyle="hsl("+getRandomInt(0,360)+",70%,50%)";
-  fillContext.fillRect(0,0,2*P,2*P);
+  fillContext.setBackground();
   //let t=4/W*Math.PI;
   let tx=1/W*Math.PI;
+  fillContext.setCircle(tx);
+  if (W>5) {
+    fillContext.setCircle(2*tx);
+  }
+  if (W>7) {
+    fillContext.setCircle(3*tx);
+  }
+  if (W>9) {
+    fillContext.setCircle(4*tx);
+  }
+/*
   let RZ=Math.abs(P*Math.cos(tx)*Math.cos(Math.sin(W*tx)));    
   fillContext.beginPath();
   fillContext.arc(P,P,RZ,0,2*Math.PI);
@@ -70,7 +94,7 @@ var getPattern=function() {
   fillContext.fill();
   if (W>5) {
     let t=2*tx;
-    RZ=Math.abs(P*Math.cos(t)*Math.cos(Math.sin(W*t)));    
+    let RZ=Math.abs(P*Math.cos(t)*Math.cos(Math.sin(W*t)));    
     fillContext.beginPath();
     fillContext.arc(P,P,RZ,0,2*Math.PI);
     fillContext.closePath();
@@ -79,7 +103,7 @@ var getPattern=function() {
   }
   if (W>7) {
     let t=3*tx;
-    RZ=Math.abs(P*Math.cos(t)*Math.cos(Math.sin(W*t)));    
+    let RZ=Math.abs(P*Math.cos(t)*Math.cos(Math.sin(W*t)));    
     fillContext.beginPath();
     fillContext.arc(P,P,RZ,0,2*Math.PI);
     fillContext.closePath();
@@ -88,59 +112,179 @@ var getPattern=function() {
   }
   if (W>9) {
     let t=4*tx;
-    RZ=Math.abs(P*Math.cos(t)*Math.cos(Math.sin(W*t)));    
+    let RZ=Math.abs(P*Math.cos(t)*Math.cos(Math.sin(W*t)));    
     fillContext.beginPath();
     fillContext.arc(P,P,RZ,0,2*Math.PI);
     fillContext.closePath();
     fillContext.fillStyle="hsl("+getRandomInt(0,360)+",70%,50%)";
     fillContext.fill();
   }
+*/
   return ctx.createPattern(fillContext.canvas,"no-repeat");
 }
 
-var randomizeFill=()=>{
-  if (C%2==1 && W>3) {
-    return [getPattern(),"black"];
-  } else {
-    return [
-      "hsl("+getRandomInt(0,360)+",90%,70%)",
-      "hsl("+getRandomInt(0,360)+",90%,70%)"
-    ];
-  }
-}
-var fs=randomizeFill();
 var fanFill={
-//  let ff=new Object();
-//  ff.fs=[];
-  type:"bin"
-/*
-  this.randomize=()=>{
-    if (C%2==1 && W>3) {
-      this.type="rad";
-      fs=[getPattern(),"black"];
+  fs:[],
+  ss:"black",
+  type:"bin",
+  randColors:(count)=>{
+    let c=[];
+    for (let i=0; i<count; i++) c.push("hsl("+getRandomInt(0,360)+",90%,70%)");
+    return c;
+  },
+  randomize:()=>{
+    if (C%2==1) {
+      if (W>3) {
+        fanFill.type="rad";
+        fanFill.fs=[getPattern(),"black"];
+      } else {
+        fanFill.type="bin";
+        fanFill.fs=fanFill.randColors(1);
+      }
     } else {
-      fs=[
-	"hsl("+getRandomInt(0,360)+",90%,70%)",
-	"hsl("+getRandomInt(0,360)+",90%,70%)"
-      ];
+      if (C==12) {
+        fanFill.type="ter";
+        fanFill.fs=fanFill.randColors(3);
+      } else {
+        fanFill.type="bin";
+        fanFill.fs=fanFill.randColors(2);
+      }
+    }
+    fanFill.ss="hsl("+getRandomInt(0,360)+",90%,70%)";
+  },
+  getFill:(i)=>{
+    if (fanFill.type=="rad") {
+      return fanFill.fs[0];
+    } else if (fanFill.type=="ter") {
+      return fanFill.fs[Math.abs((i-6))%3];
+    } else {
+      if (W==3) {
+        return fanFill.fs[0];
+      } else {
+        return fanFill.fs[i%2];
+      }
+    }
+  },
+  getStroke:()=>{
+    if (W==2) {
+      return "#777";
+    } else {
+      //if (W==3 && C%2==0) {
+      //if (W==3) {
+      //if (W==3 || W==5 || W==7) {
+      if (W%2==1 && C>2) {
+	return "#777";
+      } else {
+	return fanFill.ss;
+      }
     }
   }
-  return ff;
-*/
-};
+}
+var fanColor=fanFill;
 
-var getFill=(i)=>{
-  if (C%2==0) {
-    return fs[i%2];
-  } else {
-    return fs[0];
+//ctx.lineWidth=0;
+let fsx1="hsl("+getRandomInt(0,360)+",90%,50%)";
+let fsx2="hsl("+getRandomInt(0,360)+",90%,70%)";
+var draw=()=>{
+  ctx.clearRect(-P,-P,ctx.canvas.width,ctx.canvas.height);
+  for (let c=1; c<=C; c++) {
+    let pts=[];
+    let o=Math.cos(O%Math.PI)*c*Math.PI/C; 
+    let q=o/(c*4*Math.PI);
+    pts.push([P*Math.sin(o),P*Math.cos(o)]);
+    S=4*W;
+    for (let r=0; r<=S; r++) {
+      let Z=q*Math.sin(W*r*2*Math.PI/S);
+      let r2=Math.cos(2*r*Math.PI/S);
+      pts.push([r2*P*Math.sin(Z*2*Math.PI+o),r2*P*Math.cos(Z*2*Math.PI+o)]);
+    }
+
+//if (W==3 || W==5 || W==7) {
+//if (W%2==1) {
+if (W%2==1 && C>2) {
+  ctx.fillStyle=fsx1;
+  ctx.strokeStyle=fsx1;
+
+    ctx.beginPath();
+    ctx.moveTo(pts[2][0],pts[2][1]);
+    ctx.lineTo(pts[S/2][0],pts[S/2][1]);
+    ctx.lineTo(pts[S/2+2][0],pts[S/2+2][1]);
+    ctx.lineTo(pts[S][0],pts[S][1]);
+    ctx.closePath();
+    ctx.stroke();
+    ctx.fill();
+
+/*
+if (c%2==0) {
+  ctx.fillStyle=fsx1;
+  ctx.strokeStyle=fsx1;
+} else {
+  ctx.fillStyle=fsx2;
+  ctx.strokeStyle=fsx2;
+}
+*/
+//  S=12; //S=4*W;
+/*
+    ctx.beginPath();
+    ctx.moveTo(0,0);
+    ctx.lineTo(pts[S][0],pts[S][1]);
+    ctx.lineTo(pts[S-1][0],pts[S-1][1]);
+    ctx.closePath();
+    ctx.stroke();
+    ctx.fill();
+    ctx.beginPath();
+    ctx.moveTo(0,0);
+    ctx.lineTo(pts[S/2][0],pts[S/2][1]);
+    ctx.lineTo(pts[S/2+1][0],pts[S/2+1][1]);
+    ctx.closePath();
+    ctx.stroke();
+    ctx.fill();
+    ctx.beginPath();
+    ctx.moveTo(0,0);
+    ctx.lineTo(pts[1][0],pts[1][1]);
+    ctx.lineTo(pts[2][0],pts[2][1]);
+    ctx.closePath();
+*/
+/*
+if (c%2==0) {
+  ctx.fillStyle=fsx2;
+  ctx.strokeStyle=fsx2;
+} else {
+  ctx.fillStyle=fsx1;
+  ctx.strokeStyle=fsx1;
+}
+*/
+    ctx.stroke(); 
+    ctx.fill();
+}
+//if (W==3 && C%2==0) { ctx.strokeStyle="#777"; }
+
+    ctx.beginPath();
+    ctx.moveTo(pts[0][0],pts[0][1]);
+    for (let i=1; i<pts.length; i++) {
+      ctx.lineTo(pts[i][0],pts[i][1]);
+    }
+    ctx.closePath();
+    ctx.strokeStyle=fanFill.getStroke();
+    ctx.stroke();
+    ctx.fillStyle=fanFill.getFill(c);
+    //if (C%2==1 && W>3) {
+    if (fanFill.type=="rad") {
+      ctx.save();
+      ctx.translate(-P,-P);
+      ctx.fill("evenodd");
+      ctx.restore();
+    } else {
+      ctx.fill("evenodd");
+    }
   }
 }
 
-var draw=()=>{
-  ctx.clearRect(-P,-P,canvas.width,canvas.height);
+var draw1=()=>{
+  ctx.clearRect(-P,-P,ctx.canvas.width,ctx.canvas.height);
   for (let c=1; c<=C; c++) {
-    ctx.fillStyle=getFill(c);
+//let pts=[];
+    ctx.fillStyle=fanFill.getFill(c);
     ctx.beginPath();
     //let o=0;
     //let o=c*Math.PI/C; // tiler
@@ -148,16 +292,19 @@ var draw=()=>{
     //let o=O*c*Math.PI/C; 
     //let q=1/(4*C);
     let q=o/(c*4*Math.PI);
+//pts.push([P*Math.sin(o),P*Math.cos(o)]);
     ctx.moveTo(P*Math.sin(o),P*Math.cos(o));
     S=4*W;
     for (let r=0; r<=S; r++) {
       let Z=q*Math.sin(W*r*2*Math.PI/S);
       let r2=Math.cos(2*r*Math.PI/S);
+//pts.push([r2*P*Math.sin(Z*2*Math.PI+o),r2*P*Math.cos(Z*2*Math.PI+o));
       ctx.lineTo(r2*P*Math.sin(Z*2*Math.PI+o),r2*P*Math.cos(Z*2*Math.PI+o));
     }
     ctx.closePath();
     ctx.stroke();
-    if (C%2==1 && W>3) {
+    //if (C%2==1 && W>2) {
+    if (fanFill.type=="rad") {
       ctx.save();
       ctx.translate(-P,-P);
       ctx.fill("evenodd");
@@ -185,12 +332,12 @@ var stopped=true;
 var frac=1;
 var animate=(ts)=>{
   if (stopped) return;
-  //ctx.clearRect(0,0,canvas.width,canvas.height);
   O+=0.008;
   if (Math.abs(Math.cos(O%Math.PI))<0.01) {
     C=getRandomInt(2,20);
     W=getRandomInt(2,11);
-    fs=randomizeFill();
+    fanFill.randomize();
+    fsx1="hsl("+getRandomInt(0,360)+",90%,50%)";
   }
   draw();
   if (Math.abs(Math.sin(O%Math.PI))<0.01) {
@@ -210,80 +357,86 @@ var start=()=>{
     stopped=true;
   }
 }
-canvas.addEventListener("click", start, false);
+ctx.canvas.addEventListener("click", start, false);
 
-var getStdRange=(min,max,step)=>{
-  let sr=document.createElement("input");
-  sr.type="range";
-  sr.min=min;
-  sr.max=max;
-  sr.step=step;
-  sr.style.display="block";
-  return sr;
-}
-
-var controls=document.getElementById("controls");
-controls.append(
-  (()=>{
-    let q=getStdRange(0.001,0.2,0.001);
-    q.value=Q;
-    q.oninput=()=>{
-      Q=parseFloat(q.value);
-      draw();
-    }
-    return q;
-  })(),
-  (()=>{
-    let w=getStdRange(1,16,1);
-    w.value=W;
-    w.oninput=()=>{
-      W=parseFloat(w.value);
-      draw();
-    }
-    return w;
-  })(),
-  (()=>{
-    let s=getStdRange(4,200,1);
-    s.value=S;
-    s.oninput=()=>{
-      S=s.value;
-      draw();
-    }
-    return s;
-  })(),
-  (()=>{
-    let c=getStdRange(0,40,1);
-    c.value=C;
-    c.oninput=()=>{
-      C=c.value;
-      draw();
-    }
-    return c;
-  })(),
-  (()=>{
-    let o=getStdRange(0,10,0.001);
-    o.value=O;
-    o.oninput=()=>{
-      O=o.value;
-      draw();
-    }
-    return o;
-  })(),
-
-/*
-  (()=>{
-    let r=getStdRange(50,400,0.1);
-    r.value=RZ2;
-    r.oninput=()=>{
-      RZ2=r.value;
-      draw();
-    }
-    return r;
-  })(),
-*/
-
+body.style.display="grid";
+body.append((()=>{
+  let d=document.createElement("div");
+  var getStdRange=(min,max,step)=>{
+    let sr=document.createElement("input");
+    sr.type="range";
+    sr.min=min;
+    sr.max=max;
+    sr.step=step;
+    sr.style.display="block";
+    return sr;
+  }
+  d.style.gridColumn="2";
+  d.append(
+    (()=>{
+      let q=getStdRange(0.001,0.2,0.001);
+      q.value=Q;
+      q.oninput=()=>{
+	Q=parseFloat(q.value);
+	draw();
+      }
+      return q;
+    })(),
+    (()=>{
+      let w=getStdRange(1,16,1);
+      w.value=W;
+      w.oninput=()=>{
+	W=parseFloat(w.value);
+	draw();
+      }
+      return w;
+    })(),
+    (()=>{
+      let s=getStdRange(4,200,1);
+      s.value=S;
+      s.oninput=()=>{
+	S=s.value;
+	draw();
+      }
+      return s;
+    })(),
+    (()=>{
+      let c=getStdRange(0,40,1);
+      c.value=C;
+      c.oninput=()=>{
+	C=c.value;
+	draw();
+      }
+      return c;
+    })(),
+    (()=>{
+      let o=getStdRange(0,10,0.001);
+      o.value=O;
+      o.oninput=()=>{
+	O=o.value;
+	draw();
+      }
+      return o;
+    })(),
+  /*
+    (()=>{
+      let r=getStdRange(50,400,0.1);
+      r.value=RZ2;
+      r.oninput=()=>{
+	RZ2=r.value;
+	draw();
+      }
+      return r;
+    })(),
+  */
+    );
+    return d;
+  })()
 );
+
+//);
 
 //draw(1);
 onresize();
+fanFill.randomize();
 start();
