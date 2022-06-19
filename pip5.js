2cb892cd12e81616e29c93caad726813ccd54139
Paul Slaymaker <paul25882@gmail.com>
2022-06-19 09:47:01 -0500

    finish

--- pip5.js
+++ pip5.js
@@ -27,20 +27,19 @@ const getRandomInt=(min,max,low)=>{
   else return Math.floor(Math.random()*(max-min))+min;
 }
 
-var Circle=function(x,y,r,radius) {
+var Circle=function(x,y,r,radius,path) {
   this.x=x;
   this.y=y;
   this.radius=radius;
-  this.a=Math.atan2(y,x);
+//  this.a=Math.atan2(y,x);
   this.r=r;
+  this.path=path;
 }
 
 var getColors=()=>{
   let c=[];
   let colorCount=3;
-  //let hue=getRandomInt(0,90,true)+30;
   let hue=getRandomInt(150,360);
-//console.log("hue "+hue);
   for (let i=0; i<colorCount; i++) {
     let hd=Math.round(160/colorCount)*i+getRandomInt(-8,8);
     c.splice(getRandomInt(0,c.length+1),0,"hsl("+((hue+hd)%360)+",100%,50%)");
@@ -48,28 +47,16 @@ var getColors=()=>{
   return c;
 }
 
-let p8=new Path2D();
-p8.arc(0,0,8,0,TP);
-let p16=new Path2D();
-p16.arc(0,0,16,0,TP);
-let p24=new Path2D();
-p24.arc(0,0,24,0,TP);
-let p32=new Path2D();
-p32.arc(0,0,32,0,TP);
-let p48=new Path2D();
-p48.arc(0,0,48,0,TP);
-
-let counter=0;
-let hctr=0;
-
-const rps={8:p8,16:p16,32:p32,48:p48};
+const rps={};	// radii paths
+for (let i of [8,16,32,48]) {
+  rps[i]=new Path2D();
+  rps[i].arc(0,0,i,0,TP);
+}
 
 const hexInPath=(path,x,y,rad)=>{
   for (let j=0; j<6; j++) {
     let x2=x+HE*rad*Math.cos(j*TP/6);
     let y2=y+HE*rad*Math.sin(j*TP/6);
-//    let x2=x+1.18*rad*Math.cos(j*TP/6);	// 1.4 arbitrary
-//    let y2=y+1.18*rad*Math.sin(j*TP/6);
     if (ctx.isPointInPath(path,x2+CSIZE,y2+CSIZE)) {
       return true;
     }
@@ -80,33 +67,30 @@ const hexInPath=(path,x,y,rad)=>{
 const RP=CSIZE;
 var createPointArray=()=>{
   let pointArray=[];
-//for (let i=-388; i<389; i+=8) {
-//  for (let j=-388; j<389; j+=8) {
-for (let i=0; i<RP; i+=8) {
-  for (let j=0; j<RP; j+=8) {
-    let r=Math.pow(i*i+j*j,0.5);
-    if (r<8) continue;
-    if (r>RP-8) continue;
-    pointArray.push({"x":i,"y":j,"r":r});
+  for (let i=0; i<RP; i+=8) {
+    for (let j=0; j<RP; j+=8) {
+      let r=Math.pow(i*i+j*j,0.5);
+      if (r<8) continue;
+      if (r>RP-8) continue;
+      pointArray.push({"x":i,"y":j,"r":r});
+    }
   }
-}
   return pointArray;
 }
 
 var generateCircles=()=>{
   let ca=[];
   let pointArray=createPointArray();
-let counter=0;
-let hctr=0;
+//let counter=0;
+//let hctr=0;
   let dm=new DOMMatrix([1,0,0,1,0,0]);
   let p=new Path2D();
-  for (let i=0; i<10000; i++) {
+  for (let i=0; i<8000; i++) {
     if (pointArray.length==0) {
 //console.log("done "+i);
       ca.sort((a,b)=>{ return a.r-b.r; });
       return ca;
     }
-    //let radius=i>20?i>80?20:40:60;
     //let radius=i>40?i>400?i>2000?8:20:40:80;
     //let radius=i>32?i>400?i>2000?8:16:32:48;
     let radius=i>12?i>200?i>2000?8:16:32:48;
@@ -116,36 +100,31 @@ let hctr=0;
 
     if (pointArray[idx].r+radius>RP) {
       if (radius==8) pointArray.splice(idx,1);
-counter++;
       continue;
     }
-    if (x>0 && pointArray[idx].x-radius<0) {
+    if (x>0 && x-radius<0) {
       if (radius==8) pointArray.splice(idx,1);
-counter++;
       continue;
     }
-    if (y>0 && pointArray[idx].y-radius<0) {
+    if (y>0 && y-radius<0) {
       if (radius==8) pointArray.splice(idx,1);
-counter++;
       continue;
     }
     if (ctx.isPointInPath(p,x+CSIZE,y+CSIZE)) {
-counter++;
       pointArray.splice(idx,1);
       continue;
     }
     if (hexInPath(p,x,y,radius)) {
       if (radius==8) pointArray.splice(idx,1);
-hctr++;
       continue;
     }
     dm.e=x;
     dm.f=y;
     p.addPath(rps[radius],dm);
-    ca.push(new Circle(x,y,pointArray[idx].r,radius));
-    ca.push(new Circle(x,-y,pointArray[idx].r,radius));
-    ca.push(new Circle(-x,y,pointArray[idx].r,radius));
-    ca.push(new Circle(-x,-y,pointArray[idx].r,radius));
+    ca.push(new Circle(x,y,pointArray[idx].r,radius,rps[radius]));
+    ca.push(new Circle(x,-y,pointArray[idx].r,radius,rps[radius]));
+    ca.push(new Circle(-x,y,pointArray[idx].r,radius,rps[radius]));
+    ca.push(new Circle(-x,-y,pointArray[idx].r,radius,rps[radius]));
     pointArray.splice(idx,1);
   }
 }
@@ -158,16 +137,11 @@ const draw=()=>{
   return draw2(0);
 }
 
+
 const draw2=(swp)=>{
   let dm=new DOMMatrix([1,0,0,1,0,0]);
-  //let dms=new DOMMatrix([0.5,0,0,0.5,0,0]);
-//  let bPath=new Path2D();
-  //let gPath=new Path2D();
-  let yPath=new Path2D();
-  let rPath=new Path2D();
-  let paths=[new Path2D(),yPath,rPath];
-  let pos={8:rps[8],16:p16,32:p32};
-  let dps={8:rPath,16:yPath,32:paths[0]};
+  let paths=[new Path2D(),new Path2D(),new Path2D()];
+  let drawPaths={8:paths[2],16:paths[1],32:paths[0]};
   let dcount=0;
   let circleArray=cset[swp];
   for (let i=0; i<circleArray.length; i++) {
@@ -185,22 +159,7 @@ const draw2=(swp)=>{
     }
     dm.e=circleArray[i].x;
     dm.f=circleArray[i].y;
-    dps[circleArray[i].radius].addPath(pos[circleArray[i].radius],dm);
-/*
-//let dmx=dms.multiply(dm);
-    if (circleArray[i].radius==8) {
-      dps[circleArray[i].radius].addPath(pos[circleArray[i].radius],dm);
-      //rPath.addPath(p8,dm);
-    } else if (circleArray[i].radius==16) {
-      dps[circleArray[i].radius].addPath(pos[circleArray[i].radius],dm);
-      //yPath.addPath(p16,dm);
-    } else if (circleArray[i].radius==32) {
-      gPath.addPath(p32,dm);
-    //} else if (ca[i].radius==48) {
-    } else {
-debugger;
-    }
-*/
+    drawPaths[circleArray[i].radius].addPath(circleArray[i].path,dm);
   } 
   for (let p=0; p<3; p++) { 
     ctx.fillStyle=colorSet[swp][p];
@@ -224,9 +183,10 @@ var t=0;
 function animate(ts) {
   if (stopped) return;
   t++;
+//let dc=draw();
+//  if (dc==cset[0].length-4) { stopped=true; return; }
   if (draw()>=cset[0].length) {
     t=0;
-    //sw=++sw%2;
     cset.reverse();
     cset[0]=generateCircles();
     rTimeCircles();
@@ -242,16 +202,16 @@ const speed=2;
 var k=12;
 
 var rTimeCircles=()=>{
-  k=10+Math.abs(cset[0].length-cset[1].length)/4;
+  k=20+Math.abs(cset[0].length-cset[1].length)/2.5;
+//let speed1=speed*cset[0].length/cset[1].length;
+//let speed2=speed*cset[1].length/cset[0].length;
   for (let i=0, j=0; i<cset[0].length; i++) {
     if (i%4==0) j++;
-    cset[0][i].t=Math.round((-k-j)*speed);
+    cset[0][i].t=Math.round(-k-j*speed);
   }
-  //ost=fl?-k:0;
   for (let i=0, j=0; i<cset[1].length; i++) {
     if (i%4==0) j++;
-    //cset[1][i].t=Math.round((ost-j)*speed);
-    cset[1][i].t=-j*speed;
+    cset[1][i].t=Math.round(-j*speed);
   }
 }
 
@@ -259,5 +219,4 @@ var cset=[generateCircles(),generateCircles()];
 
 rTimeCircles();
 
-//draw2();
 start();
