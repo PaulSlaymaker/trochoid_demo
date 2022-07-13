d1e080c86695741fdb3c1bde32e6f4eccb644dd5
Paul Slaymaker <paul25882@gmail.com>
2022-07-13 14:43:32 -0500

    mostly finish

--- radbez16.js
+++ radbez16.js
@@ -1,4 +1,4 @@
-"use strict"; // Paul Slaymaker, paul25882@gmail.com
+"use strict"; // Paul Slaymaker, paul25882@gmail.com, https://codepen.io/aymak/pen/ZExpZYg
 const body=document.getElementsByTagName("body").item(0);
 body.style.background="#000";
 const TP=2*Math.PI;
@@ -10,14 +10,11 @@ const ctx=(()=>{
   body.append(d);
   let c=document.createElement("canvas");
   c.width=c.height=2*CSIZE;
-c.style.outline="1px dotted gray";
   d.append(c);
   return c.getContext("2d");
 })();
 ctx.translate(CSIZE,CSIZE);
 ctx.lineCap="round";
-//ctx.lineJoin="round";
-//ctx.rotate(TP/4);
 
 onresize=()=>{ 
   let D=Math.min(window.innerWidth,window.innerHeight)-40; 
@@ -28,6 +25,13 @@ onresize=()=>{
 const getRandomInt=(min,max,low)=>{
   if (low) return Math.floor(Math.random()*Math.random()*(max-min))+min;
   else return Math.floor(Math.random()*(max-min))+min;
+}
+
+function cFrac(frac) {
+  var e2=3*frac*Math.pow(1-frac,2)*0.2;
+  var e3=3*(1-frac)*Math.pow(frac,2)*0.8;
+  var e4=Math.pow(frac,3);
+  return e2+e3+e4;
 }
 
 var hues=[];
@@ -46,24 +50,17 @@ var getHues=()=>{
 }
 
 var setColors=()=>{
-  hues=getHues();
-  colors[0]="hsl("+hues[0]+",100%,70%)";
-  colors[1]="hsl("+hues[1]+",100%,70%)";
-  colors[2]="hsl("+hues[2]+",100%,70%)";
+  colors[0]="hsl("+hues[0]+",100%,80%)";
+  colors[1]="hsl("+hues[1]+",100%,80%)";
+  colors[2]="hsl("+hues[2]+",100%,80%)";
 //  colors[3]="hsl("+hues[3]+",100%,70%)";
 }
-
-const C=32;
 
 var Axis=function(idx) {
   this.pts=new Array(PC);
   this.pts2=new Array(PC);
-  let f=2/C;
-let a=idx*TP/C;
-  this.f1x=Math.cos(a-f);
-  this.f1y=Math.sin(a-f);
-  this.f2x=Math.cos(a+f);
-  this.f2y=Math.sin(a+f);
+  this.dof=TP*Math.random();
+  this.dof2=TP*Math.random();
   this.setPoints=()=>{
     for (let i=0; i<PC; i++) {
       let r=CSIZE*i/(PC-1);
@@ -102,51 +99,6 @@ let a=idx*TP/C;
   }
 }
 
-var radii=new Array(10);
-var setRadii=()=>{
-  for (let i=0; i<radii.length; i++) {
-    //radii[i]=10+Math.round(380*Math.random());
-    radii[i]=20+Math.round(360/radii.length*i);
-  }
-//  radii.sort((a,b)=>{ return a-b; });
-}
-setRadii(radii);
-
-//var lset=[new Array(C),new Array(C),new Array(C),new Array(C), new Array(C), new Array(C)];
-//var lset2=[new Array(C),new Array(C),new Array(C),new Array(C), new Array(C), new Array(C)];
-var lset=new Array(20);
-var lset2=new Array(20);
-var setLineSets=()=>{
-  for (let i=0; i<20; i++) {
-    lset[i]=new Array(C);
-    lset2[i]=new Array(C);
-  }
-}
-setLineSets();
-
-/*
-var ldo=[];
-for (let i=0; i<lset.length; i++) {
-  ldo[i]=100*Math.random();
-}
-var ldf=[];
-for (let i=0; i<lset.length; i++) {
-  ldf[i]=(0.5+Math.random())*[-1,1][getRandomInt(0,2)];
-}
-var ldo2=[];
-for (let i=0; i<lset.length; i++) {
-  ldo2[i]=100*Math.random();
-}
-var ldf2=[];
-var ldo3=[];
-var ldf3=[];
-for (let i=0; i<lset.length; i++) {
-  ldo3[i]=100*Math.random();
-  ldf2[i]=(0.5+Math.random())*[-1,1][getRandomInt(0,2)];
-  ldf3[i]=(0.5+Math.random())*[-1,1][getRandomInt(0,2)];
-}
-*/
-
 function start() {
   if (stopped) {
     stopped=false;
@@ -159,34 +111,40 @@ ctx.canvas.addEventListener("click", start, false);
 
 var stopped=true;
 var t=0;
-var s=0;
+var dt=0;
 var frac=0;
+var dur=300;
 function animate(ts) {
   if (stopped) return;
   t++;
-  if (t==200) {
+  dt++;
+  if (t==dur) {
     axFactors=getRandomAxialFactors();
     for (let i=0; i<lineCount; i++) aa[i].setPoints();
     frac=1;
-  } else if (t==400) {
+  } else if (t==2*dur) {
     axFactors2=getRandomAxialFactors();
     for (let i=0; i<lineCount; i++) aa[i].setPoints();
     t=0;
     frac=0;
-//    stopped=true;
-  } else if (t<200) {
-    frac=t/200;
+  } else if (t<dur) {
+    //frac=t/dur;
+    frac=cFrac(t/dur);
   } else {
-    frac=(400-t)/200;
-  //} else if (t>200) {
+    //frac=(2*dur-t)/dur;
+    frac=cFrac((2*dur-t)/dur);
   }
-//  for (let i=0; i<lineCount; i++) aa[i].setPoints();
+  if (dt%2==0) {
+    hues[0]=++hues[0]%360;
+    hues[1]=++hues[1]%360;
+    hues[2]=++hues[2]%360;
+    setColors();
+  }
   draw();
   requestAnimationFrame(animate);
 }
 
-onresize();
-
+/*
 var drawPoint=(x,y,col)=>{
   ctx.beginPath();
   ctx.moveTo(x+6,y);
@@ -195,6 +153,7 @@ var drawPoint=(x,y,col)=>{
   else ctx.fillStyle=col;
   ctx.fill();
 }
+*/
 
 const PC=5;	// number of points/radii on each axis
 //const CD=CSIZE/2/PC;
@@ -202,13 +161,18 @@ const CD=CSIZE/1.5/PC;
 
 var lineCount=8;
 
-var getAxialFactorArray=(n)=>{
+var getAxialFactorArray=(n,rad)=>{
   let fa=new Array(n);
-  for (let i=0; i<n; i++) {
-    //let z=TP/4/(n-1)*i;
-    let z=TP/4*Math.random();
+  for (let i=0; i<n-1; i++) {
+    //let z=TP/4/n*i;
+    //let z=TP/4*Math.random();
+//let rf=0.95;
+let rf=0.30;
+if (rad==PC-1) rf=0;
+let z=Math.random()<rf?TP/4/n*i:TP/4*Math.random();
     fa[i]={"x":Math.cos(z),"y":Math.sin(z),"z":z};
   }
+  fa[n-1]={"x":Math.cos(TP/4),"y":Math.sin(TP/4),"z":TP/4};
   fa.sort((a,b)=>{ return a.z-b.z; });
   return fa;
 }
@@ -216,103 +180,68 @@ var getAxialFactorArray=(n)=>{
 var getRandomAxialFactors=()=>{
   let af=new Array(PC);
   for (let i=0; i<PC; i++) {
-    let fc=Math.max(1,Math.round(i*lineCount/(PC-3)));
-    af[i]=getAxialFactorArray(fc);
+    let fc=Math.max(2,Math.round(2*i*lineCount/(PC)));
+    af[i]=getAxialFactorArray(fc,i);
   }
   return af;
 }
 var axFactors=getRandomAxialFactors();
 var axFactors2=getRandomAxialFactors();
 
-var pts=new Array(PC);
-var setPoints=(l)=>{
-//  pts[0]={"x":0,"y":0,"c2x":CD*fa[1].x,"c2y":CD*fa[1].y};
-  for (let i=0; i<PC; i++) {
-    let r=CSIZE*i/(PC-1);
-
-//let ri=getRandomInt(0,axFactors[i].length);
-    let ri=Math.trunc(l*axFactors[i].length/lineCount);
-    let xf=axFactors[i][ri].x;
-    let yf=axFactors[i][ri].y;
-
-//    let x=r*Math.cos(z);
-//    let y=r*Math.sin(z);
-    pts[i]={"x":xf*r,"y":yf*r,"c1x":xf*(r-CD),"c1y":yf*(r-CD),"c2x":xf*(r+CD),"c2y":yf*(r+CD)};
-  }
-}
-
-var opath=new Path2D();
-
-/*
-var getPath=()=>{
-  let p=new Path2D();
-  p.moveTo(pts[0].x,pts[0].y);
-//opath.moveTo(0,0);
-  for (let i=0; i<pts.length-1; i++) {
-      p.bezierCurveTo(
-        pts[i].c2x,pts[i].c2y,pts[i+1].c1x,pts[i+1].c1y,
-        pts[i+1].x,pts[i+1].y
-      );
-//      opath.bezierCurveTo(pts[i].c2x,pts[i].c2y,pts[i+1].c1x,pts[i+1].c1y,pts[i+1].x,pts[i+1].y);
-  }
-  return p;
-}
-*/
-
-var aa=new Array(lineCount);
+var aa=new Array(lineCount);	// axis array
 for (let i=0; i<lineCount; i++) {
   aa[i]=new Axis(i);
 }
-
-ctx.lineWidth=6;
-ctx.globalAlpha=0.9;
-ctx.setLineDash([400,100000]);
-ctx.fillStyle="#00000008";
 
 var draw=()=>{
   ctx.clearRect(-CSIZE,-CSIZE,2*CSIZE,2*CSIZE);
   //ctx.fillRect(-CSIZE,-CSIZE,2*CSIZE,2*CSIZE);
 
+ctx.globalAlpha=0.4;
   let opath=new Path2D();
+  let spath=new Array(lineCount);
   for (let i=0; i<lineCount; i++) {
-  //  aa[i].setPoints();
     let pth=aa[i].getPath();
     pth.addPath(pth, new DOMMatrix([1,0,0,-1,0,0]));
     pth.addPath(pth, new DOMMatrix([-1,0,0,1,0,0]));
-
-ctx.lineWidth=7;
-    ctx.strokeStyle=colors[i%colors.length];
-    ctx.stroke(pth);
-
-/*
-ctx.setLineDash([0,400,1,100000]);
-ctx.lineWidth=25;
-    ctx.strokeStyle="red";
-    ctx.stroke(pth);
-*/
-opath.addPath(pth);
+    spath[i]=pth;
+    opath.addPath(pth);
   }
 
-ctx.lineWidth=1;
-ctx.strokeStyle="black";
-ctx.stroke(opath);
- 
-/*
+  ctx.lineDashOffset=0;
+  ctx.lineWidth=34;
   for (let i=0; i<lineCount; i++) {
-  setPoints(i);
-  let pth=getPath();
-pth.addPath(pth, new DOMMatrix([1,0,0,-1,0,0]));
-pth.addPath(pth, new DOMMatrix([-1,0,0,1,0,0]));
-  //ctx.strokeStyle="green";
-  ctx.strokeStyle=colors[i%colors.length];
-  ctx.stroke(pth);
-  //  for (let i=0; i<pts.length; i++) drawPoint(pts[i].x,pts[i].y);
+    let ldo=320+60*Math.sin(dt/40+aa[i].dof);
+    if (i==lineCount-1) ldo=240+100*Math.sin(dt/80+aa[i].dof);
+    ctx.setLineDash([ldo,100000]);
+    ctx.strokeStyle=colors[i%colors.length];
+    ctx.stroke(spath[i]);
   }
-*/
+
+  ctx.lineWidth=10;
+  for (let i=0; i<lineCount; i++) {
+    let ldo=440+80*Math.sin(dt/40+aa[i].dof2);
+if (i==lineCount-1) ldo=240+100*Math.sin(dt/80+aa[i].dof2);
+ctx.setLineDash([ldo,100000]);
+    //ctx.strokeStyle=colors[(i+1)%colors.length];
+    ctx.strokeStyle="white";
+    ctx.stroke(spath[i]);
+  }
+
+  //ctx.setLineDash([400,100000]);
+  ctx.setLineDash([2,7]);
+  ctx.lineWidth=4;
+  ctx.strokeStyle="#2A2A2A";
+  ctx.lineDashOffset=-t/3;
+  ctx.globalAlpha=0.8;
+  ctx.stroke(opath);
 
 }
 
+onresize();
+
+hues=getHues();
 setColors();
 
-draw();
-//start();
+//draw();
+start();
