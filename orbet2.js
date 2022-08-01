570a4d9f77121ef247a8113692d633876b928fa0
Paul Slaymaker <paul25882@gmail.com>
2022-08-01 11:41:08 -0500

    addd to index

--- orbet2.js
+++ orbet2.js
@@ -1,7 +1,7 @@
 "use strict"; // Paul Slaymaker, paul25882@gmail.com
 const body=document.getElementsByTagName("body").item(0);
 body.style.background="#000";
-//const EM=location.href.endsWith("em");
+const EM=location.href.endsWith("em");
 const TP=2*Math.PI;
 const CSIZE=400;
 
@@ -229,6 +229,7 @@ function animate(ts) {
     colors=getColors();
     setSymmetries();
   }
+if (EM && t==200) stopped=true;
   draw();
   requestAnimationFrame(animate);
 }
