18496923a5cfc413794953d3eebc142f32252f9b
Paul Slaymaker <paul25882@gmail.com>
2022-09-08 06:14:47 -0500

    codepen url

--- symgrad4.js
+++ symgrad4.js
@@ -1,4 +1,4 @@
-"use strict"; // Paul Slaymaker, paul25882@gmail.com
+"use strict"; // Paul Slaymaker, paul25882@gmail.com, https://codepen.io/aymak/pen/KKZMraO
 const body=document.getElementsByTagName("body").item(0);
 body.style.background="#000";
 const EM=location.href.endsWith("em");
