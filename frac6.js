b1091381d749b9afe06acd8e0e9e18dda61dcd97
Paul Slaymaker <paul25882@gmail.com>
2023-11-20 13:21:06 -0600

    mods

--- frac6.js
+++ frac6.js
@@ -55,9 +55,9 @@ function Color() {
     this.fb=1-Math.pow(0.9*Math.random(),8);
   }
   this.randomize=()=>{
-    this.RK1=160+160*Math.random();
-    this.GK1=160+160*Math.random();
-    this.BK1=160+160*Math.random();
+    this.RK1=120+120*Math.random();
+    this.GK1=120+120*Math.random();
+    this.BK1=120+120*Math.random();
     this.RK2=TP*Math.random();
     this.GK2=TP*Math.random();
     this.BK2=TP*Math.random();
@@ -75,56 +75,67 @@ const colors=[new Color(),new Color()];
 var Brush=function() {
   this.getPath=()=>{
     let p=new Path2D();
-    let x=t*CSIZE/DUR;
     //let y=this.ry*Math.sin(this.k1+t/this.k2);
-    let y=this.ry*(this.yf*Math.sin(this.k1+t/this.k2)+(1-this.yf)*Math.sin(t/this.k5));
+    //let y=this.ry*(this.yf*Math.sin(this.k1+t/this.k2)+(1-this.yf)*Math.sin(t/this.k5));
     //let r=this.sr*(1+Math.cos(this.k3+t/this.k4))*(Math.cos(Math.PI/2*t/DUR));
     //let r=this.sr*Math.pow(Math.cos(this.k3+t/this.k4),2)*(Math.cos(Math.PI/2*t/DUR));
-    let r=this.sr*Math.pow(this.rf*Math.cos(this.k3+t/this.k4)+(1-this.rf)*Math.cos(t/this.k6),2)
+    let r=(6+this.sr*Math.pow(this.rf*Math.cos(this.k3+t/this.k4)+(1-this.rf)*Math.cos(t/this.k6),2))
             *(Math.cos(Math.PI/2*t/DUR));
-    p.moveTo(x,y+r);
-    p.arc(x,y,r,TP/4,3*TP/4);
-//p.ellipse(x,y,3*r,r,0,TP/4,3*TP/4);
+if (this.type) r=Math.min(r,mr);
 /*
-  p.moveTo(x-r,y);
-  p.arc(x,y,r,TP/2,3*TP/4);
-  p.moveTo(x-r,y);
-  p.arc(x,y,r,TP/2,TP/4,true);
+    p.moveTo(this.x,this.y+r);
+    p.arc(this.x,this.y,r,TP/4,3*TP/4);
 */
+//p.ellipse(x,y,3*r,r,0,TP/4,3*TP/4);
+  p.moveTo(this.x-r,this.y);
+  p.arc(this.x,this.y,r,TP/2,3*TP/4);
+  p.moveTo(this.x-r,this.y);
+  p.arc(this.x,this.y,r,TP/2,TP/4,true);
     return p;
   }
   this.randomize=()=>{
-    //this.k1=0; //TP*Math.random();
     this.k1=-TP/8+TP/4*Math.random();
     this.k2=60+100*Math.random();
-    //this.k3=TP/2; //TP*Math.random();
     this.k3=3*TP/8+TP/4*Math.random();
     //this.k4=20+100*Math.random();
-this.k4=40+200*Math.random();
-    this.ry=10+100*Math.random();
-//    this.sr=10+50*Math.random();
+    this.k4=40+200*Math.random();
+    this.ry=8+80*Math.random();
     this.sr=20+100*Math.random();
     this.yf=1-Math.pow(0.9*Math.random(),8);
-    this.k5=4+30*Math.random();
-    this.rf=1-Math.pow(0.9*Math.random(),10);
-    this.k6=4+20*Math.random();
+    this.k5=10+40*Math.random();
+    this.rf=1-Math.pow(0.9*Math.random(),8);
+    this.k6=12+60*Math.random();
+    this.type=Math.random()<0.7;
+    this.df=3+20*Math.random();
+    this.kr=20*Math.random();
+    this.eos=20*Math.random();
+    this.er=4+12*Math.random();
+    this.ga=Math.pow(0.8*Math.random(),4);
   }
   this.randomize();
+  this.move=()=>{
+    this.x=t*CSIZE/DUR;
+    this.y=this.ry*(this.yf*Math.sin(this.k1+t/this.k2)+(1-this.yf)*Math.sin(t/this.k5));
+  }
 }
 
 const dmx=new DOMMatrix([-1,0,0,1,0,0]);
 const dmy=new DOMMatrix([1,0,0,-1,0,0]);
 
 var transit=()=>{
-  //colors.forEach((c)=>{ c.randomizeF(); });
   ba.forEach((b)=>{ b.randomize(); });
   colors.forEach((c)=>{ c.randomize(); });
+  let g=getRandomInt(0,256);
 }
 
 var stopped=true;
 var start=()=>{
   if (stopped) { 
     stopped=false;
+    if (t==0) {
+      ctx.setTransform(1,0,0,1,CSIZE,CSIZE);
+      ctx.clearRect(-CSIZE,-CSIZE,2*CSIZE,2*CSIZE);
+    }
     requestAnimationFrame(animate);
   } else {
     stopped=true;
@@ -156,7 +167,8 @@ var animate=()=>{
   if (stopped) return;
   t++;
   c++;
-  //ba.forEach((b)=>{ b.move(); });
+  ba.forEach((b)=>{ b.move(); });
+setMidRadius();
   draw();
   if (t%DUR==0) {//stopped=true;
     t=0;
@@ -172,6 +184,11 @@ const dmh1=new DOMMatrix([-0.5,-0.866,0.866,-0.5,0,0]);
 const dmh2=new DOMMatrix([-0.5,0.866,-0.866,-0.5,0,0]);
 const dmh3=new DOMMatrix([0.5,0.866,-0.866,0.5,0,0]);
 
+var mr=200;
+var setMidRadius=()=>{
+  mr=Math.pow(Math.pow(ba[0].x-ba[1].x,2)+Math.pow(ba[0].y-ba[1].y,2),0.5)/2; 
+}
+
 var draw=()=>{
   for (let i=0; i<ba.length; i++) {
     let p=new Path2D(ba[i].getPath());
@@ -185,14 +202,29 @@ if (i%2) ctx.globalAlpha=0.2+0.8*Math.pow(Math.sin(t/8),8);
 else ctx.globalAlpha=0.2+0.8*Math.pow(Math.sin(t/11),8);
 */
     ctx.setTransform(1,0,0,1,CSIZE,CSIZE);
-    ctx.lineWidth=3;
+    ctx.lineWidth=4;
     ctx.strokeStyle=colors[i].getRGB();
     ctx.stroke(p2);
 
+let mark=mr/ba[i].er;//*(1+Math.sin(t/ba[i].df));
+//ctx.setLineDash([0,40-mark/2,mark,2000]);
+ctx.setLineDash([0,ba[i].eos,mark,2000]);
+ctx.strokeStyle=colors[(i+1)%2].getRGB();
+//ctx.strokeStyle="#FFFFFF20";
+//ctx.strokeStyle="#FFFFFF80";
+//ctx.strokeStyle="black";
+ctx.lineWidth=5;
+ctx.globalAlpha=ba[i].ga;
+ctx.stroke(p2);
+ctx.setLineDash([]);
+ctx.globalAlpha=1;
+
     ctx.setTransform(1,0,0,1,CSIZE-1,CSIZE+1);
-    ctx.strokeStyle="#00000018";
+    ctx.strokeStyle="#00000016";
     ctx.lineWidth=8;
     ctx.stroke(p2);
+
+
   }
 }
 
