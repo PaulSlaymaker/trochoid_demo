0afa7cb51df79bc62ea9a2e94ef3b5f8ffe3fb57
Paul Slaymaker <paul25882@gmail.com>
2025-03-16 13:04:12 -0500

    mods

--- circ71.js
+++ circ71.js
@@ -58,10 +58,16 @@ function Circle(x,y,r,a,d) {
   this.r=r;
   this.a=a;
   this.dir=d;
+this.ocp="";
   this.getCirclePath=()=>{
     let p=new Path2D();
     p.arc(x,y,r,0,TP);
     return p;
+  }
+  this.getD=()=>{ 
+    let xd=this.x-CSIZE;
+    let yd=this.y-CSIZE;
+    return Math.pow(xd*xd+yd*yd,0.5); 
   }
   this.getPathS=(da)=>{
     let p=new Path2D();
@@ -113,46 +119,72 @@ p.arc(x,y,r,(f)*az2+(1-f)*da,da);
   }
 
   this.generate2=(l)=>{		// ? shortening r, directional a
-if (l>200) {
-  console.log(l,ca.length);
-  return false;
-}
-    let ga=TP*Math.random();
+//    let gc=[];
+//    let ga=TP*Math.random();	// favor 0,TP
     //let ga=(TP*Math.random()+Math.PI)%TP;
-    //let ga=Math.PI*Math.random();
-    if (this.a>ga+Math.PI) ga+=Math.PI/2;
-    if (this.a>ga+Math.PI) ga+=Math.PI/4;
+let ga=(this.a+TP*Math.random())%TP;
+//    if (this.a>ga+Math.PI) ga+=Math.PI/2;
+//    if (this.a>ga+Math.PI) ga+=Math.PI/4;
+if (this.a>ga+Math.PI) {
+  this.di=(this.a-ga+Math.PI).toFixed(1);
+  ga+=8*Math.PI/8;	// over circled
+  //ga+=4*Math.PI/4;	
+  this.ocp="p";
+}
+if (this.a<ga-Math.PI) {
+  ga-=8*Math.PI/8;
+  this.ocp="n";
+}
+
+if (ga<0) ga+=TP;
+else ga%=TP;
+//if (this.a>ga+Math.PI) ga+=Math.PI/2;
 /*
     if (this.a>ga+Math.PI) {	// over circled
 console.log("oc",l,ca.length,ga.toFixed(1));
       return this.generate2(l+1);
     }
 */
-    if (this.a+Math.PI<ga) ga-=Math.PI/2;
-    if (this.a+Math.PI<ga) ga-=Math.PI/4;
 //console.log(ca[1].a>ca[0].a+Math.PI);
     //let gr=MINR+(MAXR-MINR)*Math.random();
     let gr=MINR+(MAXR-MINR)*Math.pow(Math.random(),l+1);
     let gx=this.x+(this.r+gr)*Math.cos(ga);
     let gy=this.y+(this.r+gr)*Math.sin(ga);
     if (checkCircle(gx,gy,gr)) {
+//gc.push([Math.pow(gx*gx+gy*gy,0.5),gx,gy,gr,ga]);
+//gc.push(new Circle(gx,gy,gr,ga,-this.dir));
 //console.log(l);
 ES=l;
       return new Circle(gx,gy,gr,ga,-this.dir);
     } else {
+
+if (l>100) {
+  console.log(l,ca.length);
+  return false;
+}
       return this.generate2(l+1);
     }
   }
 }
 
+
+ctx.font="bold 11px sans-serif";
+ctx.textAlign="center";
 var showC=()=>{
   ctx.lineWidth=1;
-  ctx.strokeStyle="yellow";
-  ctx.stroke(ca[0].getCirclePath());
+//  ctx.strokeStyle="yellow";
+//  ctx.stroke(ca[0].getCirclePath());
   ctx.strokeStyle="silver";
-  for (let i=1; i<ca.length; i++) {
+  ctx.fillStyle="white";
+  for (let i=0; i<ca.length; i++) {
     ctx.stroke(ca[i].getCirclePath());
+    //let rep=i+" "+ca[i].a.toFixed(1);
+    ctx.fillText(i,ca[i].x,ca[i].y-7);
+    ctx.fillText(ca[i].a.toFixed(2),ca[i].x,ca[i].y+3);
+    ctx.fillText(ca[i].ocp,ca[i].x,ca[i].y+13);
   }
+  ctx.arc(CSIZE,CSIZE,CSIZE,0,TP);
+  ctx.stroke();
 }
 
 var f=1;
@@ -179,7 +211,8 @@ ctx.lineWidth=8;
 }
 
 var stopped=true;
-const DUR=48;
+//const DUR=36;
+const DUR=20;
 var start=()=>{
   if (stopped) { 
     stopped=false;
@@ -200,12 +233,12 @@ var animate=(ts)=>{
 //    stopped=true
     if (!grow()) {
       showC();
-      return;
+return;	// test
     }
     t=0;
   }
-if (ca.length<2) stopped=true;
   draw();
+if (ca.length<2) stopped=true;
   requestAnimationFrame(animate);
 }
 
@@ -230,24 +263,35 @@ var checkCircle=(x,y,r)=>{	// ? remove corners, spacer+lineWidth
 onresize();
 
 const MAXR=80;
-const MINR=16;
-const COUNT=getRandomInt(5,10);
-console.log("count",COUNT);
-
+const MINR=20;
+//const MINR=16;
+//const MINR=32;
+const COUNT=4; //getRandomInt(5,12);
+//console.log("count",COUNT);
 
 let x=MAXR+2*(CSIZE-MAXR)*Math.random();
 let y=MAXR+2*(CSIZE-MAXR)*Math.random();
 let r=MINR+(MAXR-MINR)*Math.random();
-let a=TP*Math.random();
+let a=Math.PI/4; //0.3+0.7*TP*Math.random();
 
+x=370;
+y=60;
+r=20;
+a=0.1;
 var ca=[new Circle(x,y,r,a,1)];
+
+/*
+ca[0].x=370;
+ca[0].y=60;
+ca[0].r=20;
+ca[0].a=0;
+*/
 
 for (let i=0; i<COUNT; i++) {
   let circle=ca[i].generate2(0);
   //if (!circle) break;
-if (!circle) debugger;
+if (!circle) break;	// test for very short ca
   ca.push(circle);
-//  ca[i+1]=ca[i].generate2(0);
 }
 
 //ca.shift();
@@ -262,7 +306,17 @@ ctx.stroke(new Path2D(ca[ca.length-3].getPath(ca[ca.length-2].a,1)));
 var grow=()=>{
   let circle=ca[ca.length-1].generate2(0);
   if (circle) {
-//    if (ca.length>10) 
+    let circle2=ca[ca.length-1].generate2(0);
+    if (circle2) {
+//console.log(circle.getD().toFixed(1),circle2.getD().toFixed(1));
+      if (circle.getD()>circle2.getD()) {
+        circle=circle2;
+      }
+      let circle3=ca[ca.length-1].generate2(0);
+      if (circle3) {
+	if (circle.getD()>circle3.getD()) circle=circle3;
+      }
+    }  // else
     ca.shift();
     ca.push(circle);
     return true;
