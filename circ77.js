88a4d4b529b00a0c1264fa69ccdf89acd92f7684
Paul Slaymaker <paul25882@gmail.com>
2025-09-28 09:22:38 -0500

    mods

--- circ77.js
+++ circ77.js
@@ -17,6 +17,7 @@ c.style.outline="1px dotted gray";
   return c.getContext("2d");
 })();
 ctx.translate(CSIZE,CSIZE);
+ctx.lineCap="round";
 
 onresize=()=>{ 
   let D=Math.min(window.innerWidth,window.innerHeight)-40; 
@@ -28,13 +29,18 @@ const getRandomInt=(min,max,low)=>{
   else return Math.floor(Math.random()*(max-min))+min;
 }
 
+const CCOUNT=1;
+
 function Color(km,kv) {
-  const CBASE=174;
+  const CBASE=160;
   const CT=255-CBASE;
   this.getRGB=()=>{
-    let red=Math.round(CBASE+CT*Math.cos(this.RK2+t/this.RK1));
-    let grn=Math.round(CBASE+CT*Math.cos(this.GK2+t/this.GK1));
-    let blu=Math.round(CBASE+CT*Math.cos(this.BK2+t/this.BK1));
+    let red=Math.round(CBASE+CT*Math.random());
+    let grn=Math.round(CBASE+CT*Math.random());
+    let blu=Math.round(CBASE+CT*Math.random());
+    //let red=Math.round(CBASE+CT*Math.cos(this.RK2+t/this.RK1));
+    //let grn=Math.round(CBASE+CT*Math.cos(this.GK2+t/this.GK1));
+    //let blu=Math.round(CBASE+CT*Math.cos(this.BK2+t/this.BK1));
     return "rgb("+red+","+grn+","+blu+")";
   }
   this.randomize=()=>{
@@ -48,11 +54,14 @@ function Color(km,kv) {
   this.randomize();
 }
 
-var color=new Color(200,400);
-var color2=new Color(40,80);
+var color=new Color();
+var col=[];
+for (let i=0; i<CCOUNT; i++) {
+  col.push(color.getRGB());
+}
 
-//const MAXR=80;
-const MAXR=40;
+const MAXR=80;
+//const MAXR=40;
 var s=0.5;
 
 var getAngle=(x,y,r,a,d)=>{	// not especially effective for central trend
@@ -71,6 +80,7 @@ var getAngle=(x,y,r,a,d)=>{	// not especially effective for central trend
 var D;
 
 function Circle(gc,timp,d) {
+//  this.curve=curve;
   this.gc=gc;
   if (gc==0) {
     this.dir=d;
@@ -97,62 +107,50 @@ function Circle(gc,timp,d) {
 //    this.yp1=this.y+this.r*Math.sin(this.a1);
   }
   this.setAR();
-/*
   this.getCirclePath=()=>{
     let p=new Path2D();
     p.arc(this.x,this.y,this.r,0,TP);
     return p;
   }
-*/
+/*
   this.setCirclePath=(p)=>{
-//    let xp=this.x+this.r*Math.cos(this.a1);
-//    let yp=this.y+this.r*Math.sin(this.a1);
     p.moveTo(this.xp,this.yp);
     p.arc(this.x,this.y,this.r,this.a1,this.a1+TP);
   }
+*/
   this.setPath=(p)=>{
-//    if (fc) p.arc(this.x,this.y,this.r,this.a1,this.a1+TP); else 
     p.arc(this.x,this.y,this.r,this.a1,this.a2,this.dir==-1);
   }
-  this.setPathT=(p)=>{
-    //let d=t/DUR*(this.a2-this.a1);
-    let d=(t-this.time)/(s*this.dist)*(this.a2-this.a1);
+  this.setPathT=(cur)=>{
+    let d=(cur.t-this.time)/(s*this.dist)*(this.a2-this.a1);
+    cur.path.arc(this.x,this.y,this.r,this.a1,this.a1+d,this.dir==-1);
 /*
-    let and2=this.dir*(this.a2-this.a1);
-    if (and2<0) {
-      p.arc(this.x,this.y,this.r,this.a1,this.a1-d+TP,this.dir==-1);
-    } else {
-      p.arc(this.x,this.y,this.r,this.a1,this.a1+d,this.dir==-1);
+    if (t-this.time>s*this.dist) {
+      let cir=new Circle(this,t);
+      if (!cir.inbound) {
+	curve.state="S";
+      } else {
+        curve.ca.push(cir);
+      }
     }
 */
-    p.arc(this.x,this.y,this.r,this.a1,this.a1+d,this.dir==-1);
-    if (t-this.time>s*this.dist) {
-    let cir=new Circle(this,t);
-    if (!cir.inbound) {
-//      stopped=true;
-//      report();
-      state="S";
-//s=4;
-    }
-    else curve.ca.push(cir);
-    }
   }
-  this.setPathE=(p)=>{
-if (state=="G") D=0;
-else    D=(t-this.time)/(s*this.dist)*(this.a2-this.a1);
+  this.setPathE=(cur)=>{
+    if (cur.state=="G") D=0;
+    else D=(cur.t-this.time)/(s*this.dist)*(this.a2-this.a1);
     let xp=this.x+this.r*Math.cos(this.a1+D);
     let yp=this.y+this.r*Math.sin(this.a1+D);
 //drawPoint(xp,yp,"yellow",5);
 //drawPoint(this.xp,this.yp,"blue");
-    p.moveTo(xp,yp);
-if (state=="G") {
-  p.arc(this.x,this.y,this.r,this.a1,this.a2,this.dir==-1);
-  return true;
-}
-    if (t-this.time>s*this.dist) {
+    cur.path.moveTo(xp,yp);
+    if (cur.state=="G") {
+      cur.path.arc(this.x,this.y,this.r,this.a1,this.a2,this.dir==-1);
       return true;
     }
-    p.arc(this.x,this.y,this.r,this.a1+D,this.a2,this.dir==-1);
+    if (cur.t-this.time>s*this.dist) {
+      return true;
+    }
+    cur.path.arc(this.x,this.y,this.r,this.a1+D,this.a2,this.dir==-1);
     return false;
   }
   this.getCirclePath=()=>{
@@ -165,30 +163,47 @@ if (state=="G") {
     let at=this.a1;
     this.a1=this.a2;
     this.a2=at;
+//if (this.dir*(this.a2-this.a1)<0) debugger;
     this.time=0;
   }
-  this.inbound=true;
-  let xp2=this.x+this.r*Math.cos(this.a2);
-//  if (Math.abs(xp2)>CSIZE-MAXR) this.inbound=false;
-  let yp2=this.y+this.r*Math.sin(this.a2);
-//  if (Math.abs(yp2)>CSIZE-MAXR) this.inbound=false;
-  let dc=Math.pow(xp2*xp2+yp2*yp2,0.5);
-  if (dc>CSIZE-MAXR) this.inbound=false;
+  this.checkBounds=()=>{ // make global
+    //this.inbound=true;
+    let xp2=this.x+this.r*Math.cos(this.a2);
+  //  if (Math.abs(xp2)>CSIZE-MAXR) this.inbound=false;
+    let yp2=this.y+this.r*Math.sin(this.a2);
+  //  if (Math.abs(yp2)>CSIZE-MAXR) this.inbound=false;
+    for (let i=-1; i<2; i++) {
+      let xpt=xp2+3*i;
+      for (let j=-1; j<2; j++) {
+        let ypt=yp2+3*j;
+	let dc=Math.pow(xpt*xpt+ypt*ypt,0.5);
+	//if (dc>CSIZE-MAXR) this.inbound=false;
+	if (dc>CSIZE) return false;
+      // use circles instead of path, or in addition?, at least 5 indexes away?
+	for (let k=0; k<cua.length; k++) {
+	  if (ctx.isPointInPath(cua[k].path,xpt+CSIZE,ypt+CSIZE)) return false;
+	}
+      }
+    }
+    return true;
+  }
 }
 
-function Curve() {
-  this.ca=[];
+function Curve(count) {
+//  this.count=count;
+  this.t=0;
   this.path=new Path2D();
+  this.state="N";
+  this.ca=[];
+  this.ca.push(new Circle(0,0,1));
   this.setPath=()=>{
-//    for (let i=0; i<this.ca.length; i++) this.ca[i].setAR();
     this.path=new Path2D();
-
-    let notail=this.ca[0].setPathE(this.path);
+    let notail=this.ca[0].setPathE(this);
 //    let xp=this.ca[0].x+this.ca[0].r*Math.cos(this.ca[0].a1);
 //    let yp=this.ca[0].y+this.ca[0].r*Math.sin(this.ca[0].a1);
 //    this.path.moveTo(this.ca[0].xp,this.ca[0].yp);
 
-    if (state=="S") {
+    if (this.state=="S") {
       for (let i=1; i<this.ca.length; i++) {
 	this.ca[i].setPath(this.path);
       }
@@ -197,24 +212,37 @@ function Curve() {
 	this.ca[i].setPath(this.path);
       }
     }
-
-
-    if (state=="N" || state=="G") {
-      this.ca[this.ca.length-1].setPathT(this.path);
+    if (this.state=="N" || this.state=="G") {
+      let fc=this.ca[this.ca.length-1];
+      fc.setPathT(this);
+      if (this.t-fc.time>=s*fc.dist) {
+	let cir=new Circle(fc,this.t);
+	if (cir.checkBounds()) {
+	  this.ca.push(cir);
+	} else {
+if (notail) console.log(this);
+if (notail) this.state="R";
+//if (notail) report();
+//if (notail) debugger;
+//if (notail) stopped=true;
+if (notail) return;
+	}
+      }
     }
     if (notail) {
-      if (state=="S") {
-        state="R";
+      if (this.state=="S") {
+        this.state="R";
 //stopped=true;
 //report();
-      } else if (state=="G") {
-        if (this.ca.length>COUNT) state="N";
-	this.ca[0].time=t;
+      } else if (this.state=="G") {
+//        if (this.ca.length>count) this.state="N";
+if (this.ca.length>=count) this.state="N";
+	this.ca[0].time=this.t;
       } else {
-//if (state!="N") debugger;
+//if (this.state=="R") debugger;
 	this.ca.shift();
-	this.ca[0].time=t;
-        if (this.ca.length<COUNT) state="G";
+	this.ca[0].time=this.t;
+        if (this.ca.length<count) this.state="G";
       }
     }
   }
@@ -231,19 +259,17 @@ function Curve() {
   this.reverse=()=>{
     for (let i=0; i<this.ca.length; i++) this.ca[i].reverse();
     this.ca.reverse();
-    state="N";
+//this.ca[this.ca.length-1].time=s*this.ca[this.ca.length-1].dist;
+    this.state="N";
+    this.t=0;
   }
+for (let i=0; i<count; i++) {
+  let c=new Circle(this.ca[i],0);
+  if (c.checkBounds()) this.ca.push(c);
+  else break;
 }
-
-/*
-var checkR=()=>{
-  let c=curve.ca[curve.ca.length-1];
-  let xp2=c.x+c.r*Math.cos(c.a2);
-  if (Math.abs(xp2)>CSIZE-MAXR) stopped=true;
-  let yp2=c.y+c.r*Math.sin(c.a2);
-  if (Math.abs(yp2)>CSIZE-MAXR) stopped=true;
+  this.setPath();
 }
-*/
 
 const dm1=new DOMMatrix([-1,0,0,1,0,0]);
 const dm2=new DOMMatrix([1,0,0,-1,0,0]);
@@ -284,46 +310,53 @@ var start=()=>{
 }
 body.addEventListener("click", start, false);
 
-var state="N";
-var DUR=300;
-var t=0;
 var animate=(ts)=>{
   if (stopped) return;
-  t++;
-//  if (t>=DUR) stopped=true;
-//  if (t>curve.ca[curve.ca.length-1].r) stopped=true;
+  cua.forEach((c)=>{ c.t++; c.setPath(); });
+  cua.forEach((c)=>{ 
+    if (c.state=="R") {
+//      c.t=0;
+      c.reverse();
+    }
+if (c.ca.length<3) stopped=true;
+  });
   draw();
-  if (state=="R") {
-    curve.reverse();
-    t=0;
-  }
-if (curve.ca.length<3) stopped=true;
   requestAnimationFrame(animate);
 }
 
-const COUNT=10;
-var curve=new Curve();
-curve.ca.push(new Circle(0,0,1));
-for (let i=0; i<COUNT; i++) {
-  curve.ca.push(new Circle(curve.ca[i],0));
+var cua=[];
+//var curve=new Curve(10);
+//cua.push(curve);
+for (let i=0; i<CCOUNT; i++) {
+  let cur=new Curve(4);
+  if (cur.ca.length>3) {
+    cua.push(cur);
+  }
 }
+/*
+for (let i=0; i<curve.count; i++) {
+  let c=new Circle(curve.ca[i],0);
+  if (c.checkBounds()) curve.ca.push(c);
+  else break;
+}
+*/
 
-ctx.lineWidth=3;
-ctx.strokeStyle=color.getRGB();
+ctx.lineWidth=6;
 var draw=()=>{
   ctx.clearRect(-CSIZE,-CSIZE,2*CSIZE,2*CSIZE);
-  curve.setPath();
-  let p=new Path2D(curve.path);
+  for (let i=0; i<cua.length; i++) {
+//  cua[i].setPath();
+  let p=new Path2D(cua[i].path);
 //p.addPath(p,dm1);
 //p.addPath(p,dm2);
 //p.addPath(p,dm3);
-/*
-  let p2=new Path2D(p);
-  p2.addPath(p,dm60);
-  p2.addPath(p,dm120);
-*/
-//ctx.lineWidth=3;	// temp?
+//  let p2=new Path2D(p);
+//  p2.addPath(p,dm60);
+  //p2.addPath(p,dm120);
+ctx.lineWidth=6;	// temp?
+ctx.strokeStyle=col[i];
   ctx.stroke(p);
+  }
 //  curve.drawEnds();
 }
 
@@ -338,6 +371,7 @@ var report=()=>{
 //  ctx.stroke(ca[0].getCirclePath());
   ctx.strokeStyle="silver";
   ctx.fillStyle="white";
+let curve=cua[0];
   for (let i=0; i<curve.ca.length; i++) {
     let c=curve.ca[i];
     ctx.stroke(c.getCirclePath());
@@ -351,3 +385,11 @@ var report=()=>{
     drawPoint(xp2,yp2,"yellow",4);
   }
 }
+var report2=()=>{
+  for (let i=0; i<cua.length; i++) {
+    ctx.fillStyle="#88880088";
+    ctx.fill(cua[i].path);
+  }
+}
+
+// normal arc test on state change
